8b28f0f8bc6568356af3d1a57ede9f087af60ab3
Leo Dorantes <leodoor2@gmail.com>
2024-11-28 10:29:00 -0600

    Implement basic multithreading example with Runnable interface

--- School_Projects/term7/SO/MultithreadingDemo.java
+++ School_Projects/term7/SO/MultithreadingDemo.java
@@ -1,1 +1,43 @@
-Hello
+class Task implements Runnable {
+    private String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread " + name + " running iteration " + i);
+            try {
+                Thread.sleep(1000); // Simulates work with a 1-second pause
+            } catch (InterruptedException e) {
+                System.out.println("Thread " + name + " was interrupted.");
+            }
+        }
+        System.out.println("Thread " + name + " finished.");
+    }
+}
+
+public class MultithreadingDemo {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Task("A"));
+        Thread thread2 = new Thread(new Task("B"));
+        Thread thread3 = new Thread(new Task("C"));
+
+        System.out.println("Starting threads...");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted.");
+        }
+
+        System.out.println("All threads have finished.");
+    }
+}
