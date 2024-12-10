f0e5a68b64319ff6805b3cb7c000197a94669b9f
Kidden0 <sebenroque@gmail.com>
2024-12-09 20:08:00 -0600

    Defined Ticket table structured

--- School_Projects/term7/DevWebA/eventify/database/migrations/2024_12_10_020056_create_tickets_table.php
+++ School_Projects/term7/DevWebA/eventify/database/migrations/2024_12_10_020056_create_tickets_table.php
@@ -4,24 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateTicketsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('event_id')->constrained()->onDelete('cascade');
+            $table->string('attendee_name');
+            $table->string('attendee_email');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('tickets');
-    }
-};
+}
+}
