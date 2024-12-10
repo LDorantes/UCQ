2cbfb24c49a9874f5bf6daff992a53fdf3422b43
Leo Dorantes <leodoor2@gmail.com>
2024-12-09 20:33:04 -0600

    model Notifications created and ticket controller has configurated to work with notifications model

--- School_Projects/term7/DevWebA/eventify/app/Http/Controllers/TicketController.php
+++ School_Projects/term7/DevWebA/eventify/app/Http/Controllers/TicketController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
+use App\Models\Ticket;
+use App\Notifications\TicketPurchasedNotification;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $ticket = new Ticket();
+        $ticket->event_id = $request->event_id;
+        $ticket->attendee_name = $request->attendee_name;
+        $ticket->attendee_email = $request->attendee_email;
+        $ticket->save();
+
+        $event = Event::find($request->event_id);
+        
+        // Enviar notificación al usuario
+        $user = auth()->user();
+        $user->notify(new TicketPurchasedNotification($event));
+
+        return redirect()->route('tickets.index');
+    }
 }
+
---
+++ School_Projects/term7/DevWebA/eventify/app/Notifications/TicketPurchasedNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class TicketPurchasedNotification extends Notification
+{
+    use Queueable;
+
+    protected $event;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($event)
+    {
+        $this->event = $event;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('Ticket Purchase Confirmation')
+                    ->greeting('Hello!')
+                    ->line('Thank you for purchasing a ticket for ' . $this->event->name . '.')
+                    ->line('Event Details:')
+                    ->line('Date: ' . $this->event->date)
+                    ->line('Location: ' . $this->event->location)
+                    ->action('View Event', url('/events/' . $this->event->id))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
