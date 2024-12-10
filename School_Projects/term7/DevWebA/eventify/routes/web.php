e5487d25c4509bc1041d3f8f3469b11d5eff2a40
Kidden0 <sebenroque@gmail.com>
2024-12-09 20:26:59 -0600

    Routes configurated

--- School_Projects/term7/DevWebA/eventify/routes/web.php
+++ School_Projects/term7/DevWebA/eventify/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\TicketController;
+
+Route::resource('events', EventController::class);
+Route::resource('tickets', TicketController::class);
+
 
 Route::get('/', function () {
     return view('welcome');
